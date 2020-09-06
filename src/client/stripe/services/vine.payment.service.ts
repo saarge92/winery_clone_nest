2edf72e7babf21c6c396d4e89699470985b25173
serdar <sarage92@mail.ru>
2020-09-06 17:35:55 +0400

    Error parsing when stripe returns error

--- src/client/stripe/services/vine.payment.service.ts
+++ src/client/stripe/services/vine.payment.service.ts
@@ -4,8 +4,12 @@ import { VineSellDto } from '../dto/vine.sell.dto';
 import Stripe from 'stripe';
 import { ConfigService } from '@nestjs/config';
 import { VineCheckRules } from '../../services/vine.check_validation';
-import { throwError } from 'rxjs';
 
+/**
+ * Service for payment for vine
+ * Here we use stripe for payment service
+ * @author Serdar Durdyev
+ */
 @Injectable()
 export class VineStripeService {
   private readonly publicApiKey: string;
@@ -18,17 +22,38 @@ export class VineStripeService {
     this.privateApiKey = configService.get<string>('STRIPE_PRIVATE_KEY');
   }
 
-  public async buyVine(vineSellInfo: VineSellDto) {
+  /**
+   * Process of selling vine
+   * @param vineSellInfo Data transfer object which contains info  about vines selling
+   */
+  public async buyVine(vineSellInfo: VineSellDto): Promise<Stripe.Charge> {
     const checkValidationVines = await this.vineCheckService.chechkVinesAndCalculate(vineSellInfo.vines);
     if (!checkValidationVines[0])
       throw new ConflictException('Неверно указаны вина в списке. Проверьте вина');
-    const token = await this.createTokenForPayInfo(vineSellInfo);
-    const charge = await this.createCharge(checkValidationVines[1], token).catch(error => {
-      throw new ConflictException(error);
+
+    const token = await this.createTokenForPayInfo(vineSellInfo).catch(error => {
+      throw new ConflictException({
+        message: error.message,
+        type: error.rawType,
+      });
     });
+
+    const charge = await this.createCharge(checkValidationVines[1], token).catch(error => {
+      throw new ConflictException({
+        message: error.message,
+        type: error.rawType,
+      });
+    });
+
     return charge;
   }
 
+  /**
+   * Sending token with card data for verification data
+   * about card
+   * @param vineSellInfo Data transfer object about order
+   * @return {Promise<Stripe.Token>} Returns generated token
+   */
   private async createTokenForPayInfo(vineSellInfo: VineSellDto): Promise<Stripe.Response<Stripe.Token>> {
     const response = await this.stripeClient.tokens.create({
       card: {
@@ -41,13 +66,16 @@ export class VineStripeService {
       },
     }, {
       apiKey: this.publicApiKey,
-    }).catch(error => {
-      throw new ConflictException(error);
     });
     return response;
   }
 
-  private async createCharge(amount: number, token: Stripe.Token) {
+  /**
+   * Create payment
+   * @param amount Summary money to pay
+   * @param token Verification data for payment
+   */
+  private async createCharge(amount: number, token: Stripe.Token): Promise<Stripe.Charge> {
     const charge = await this.stripeClient.charges.create({
       amount: amount * 100,
       currency: 'usd',
